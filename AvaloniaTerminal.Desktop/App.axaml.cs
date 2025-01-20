718df9a683b96e4a607fa9007bfe5403b7773737
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2025-01-20 11:26:36 -0800

    rename

--- AvaloniaTerminal.Desktop/App.axaml.cs
+++ AvaloniaTerminal.Desktop/App.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
-namespace AvaloniaConsole
+namespace AvaloniaTerminal
 {
     public partial class App : Application
     {
--- AvaloniaTerminal.Desktop/Program.cs
+++ AvaloniaTerminal.Desktop/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Avalonia;
 using System;
 
-namespace AvaloniaConsole
+namespace AvaloniaTerminal
 {
     internal class Program
     {
